c79528da22b8a18a45695395a8c7c5adb540ba2a
Everton Kunz <ehokeverton@gmail.com>
2019-03-22 19:13:27 -0300

    Changes in AddEntry-methods

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import { View } from 'react-native'
+
+//Components
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello App!</Text>
-        <Ionicons name='md-pizza'/>
+      <View>
+        <AddEntry />
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
