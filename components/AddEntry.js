d7f12b5084228116c95550c77241b50495eb89b8
Everton Kunz <ehokeverton@gmail.com>
2019-03-22 23:53:32 -0300

    Changes for AddEntry-SubmitBtn

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 
 //Components
 import UdaciSlider from './UdaciSlider'
 import UdaciStepers from './UdaciStepers'
+import DateHeader from './DateHeader'
+
+function SubmitBtn ({ onPress }) {
+  
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+    >
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -43,12 +55,34 @@ export default class AddEntry extends Component {
       [metric]: value,
     })
   }
+
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    })
+
+    // Navigate to Home
+
+    // Save to DB
+
+    // Clearn local notification
+  }
   
   render (){
     const metaInfo = getMetricMetaInfo()
 
     return (
       <View>
+        <DateHeader date={(new Date()).toLocaleDateString()}/>
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest} = metaInfo[key]
           const value = this.state[key]
@@ -72,6 +106,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     )
   }
