19dceefed661ec6734b38b4259c2ca55bbde72e6
Everton Kunz <ehokeverton@gmail.com>
2019-03-23 11:14:50 -0300

    Changes in UdaciStepers

--- components/UdaciStepers.js
+++ components/UdaciStepers.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default function UdaciStepers () {
+export default function UdaciStepers ({ value, max, step, unit, onIncrement, onDecrement}) {
   return (
     <View>
-      <Text>UdaciStepers</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement} size={30} color='black'>
+          <FontAwesome name='minus'/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement} size={30} color='black'>
+          <FontAwesome name='plus'/>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
