069b9f75de1ee925cf7b4ee8b08fd761c7b421a5
Everton Kunz <ehokeverton@gmail.com>
2019-03-30 02:53:57 -0300

    Changes for style-metainfo.

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
+        <View style={{flex: 1}}>
           <AddEntry />
         </View>
       </Provider>
